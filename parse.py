9ba302a5707f0a24ebf9eccd4f4ea46718d893b2
amir-zeldes <amir-zeldes@users.noreply.github.com>
2024-08-05 17:56:16 -0400

    improve parser NP patterns

--- parse.py
+++ parse.py
@@ -4,16 +4,16 @@ from nltk.tree import Tree
 stanza.download('en')  # Download English models
 nlp = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency')
 
+
 def extract_noun_phrases(tree):
     noun_phrases = []
     for subtree in tree.subtrees():
-        if subtree.label() == 'NP':
+        # Find NP or ADJP modifiers headed by noun (with NN but without JJ)
+        pos_tags = [tok[1][:2] for tok in subtree.pos()]
+        if subtree.label() in ['NP','NML'] or (subtree.label() == 'ADJP' and 'NN' in pos_tags and not any([t.startswith('J') for t in pos_tags])):
             leaves = subtree.leaves()
             # Filter out pronouns: should think of better ways
-            if all(not word.lower() in ['i', 'me', 'my', 'mine', 'we', 'us', 'our', 'ours', 
-                                        'you', 'your', 'yours', 'he', 'him', 'his', 'she', 
-                                        'her', 'hers', 'it', 'its', 'they', 'them', 'their', 
-                                        'theirs', 'this', 'that', 'these', 'those'] for word in leaves):
+            if not all([tag in ["DT","PRP","PRP$"] for tag in pos_tags]):
                 noun_phrases.append(' '.join(leaves))
     return noun_phrases
 
