14f88c63e37e4a58edf9373b18815301cf3c07d6
Jessica Lin <32357445+jl908069@users.noreply.github.com>
2024-08-18 12:06:19 +0800

    add scoring

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from get_summary import get_summary, read_documents
 from parse import parse_summaries
 from align import align, extract_mentions_from_gold_tsv
 from serialize import add_summaries_to_xml, add_anno_to_tsv
-
+from score import get_sal_tsv, get_sal_mentions, sal_coref_cluster, extract_first_mentions, calculate_scores
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_summaries", type=int, default=4, help="Number of summaries to generate")
     parser.add_argument("--alignment_component", choices=["LLM", "LLM_hf", "string_match", "coref_system"], default="string_match", help="Component to use for alignment")
-    parser.add_argument("--model_name", default="google/flan-t5-base", help="Huggingface model name to use for summarization and/or alignment with component LLM_hf")
+    parser.add_argument("--model_name", default="google/flan-t5-xl", help="Huggingface model name to use for summarization and/or alignment with component LLM_hf")
     parser.add_argument("--data_folder", type=str, default="data", help="Path to the data folder containing TSV, conllu or xml files")
     parser.add_argument("--max_docs", type=int, default=None, help="Maximum number of documents to processe (default: None = all; choose a small number to prototype)")
     parser.add_argument("--overwrite_cache", action="store_true", help="Overwrite cached summaries (default: False)")
@@ -19,6 +19,7 @@ def main():
 
     # Input data folder
     pred_tsv_folder =args.data_folder + '/output/pred_tsv'
+    # gold_tsv_folder = args.data_folder + '/input/tsv'
 
     folders_with_pred_tsv = [os.path.join(pred_tsv_folder, f'tsv_pred_train{i}') for i in range(1, args.n_summaries + 1) if glob.glob(os.path.join(pred_tsv_folder, f'tsv_pred_train{i}', '*.tsv'))] #default to train
 
@@ -27,7 +28,11 @@ def main():
     if args.max_docs is not None:
         doc_ids = doc_ids[:args.max_docs]
         doc_texts = doc_texts[:args.max_docs]
-    
+
+    gold_sal_ents=get_sal_tsv(doc_ids)
+    sal_mentions=get_sal_mentions(doc_ids)
+    sc=sal_coref_cluster(sal_mentions)
+
     # Extract mentions from gold TSV folder
     all_mentions_from_tsv = extract_mentions_from_gold_tsv(args.data_folder + '/input/tsv/', docnames=doc_ids)
 
@@ -40,7 +45,11 @@ def main():
     # Detect which entities from the document are mentioned in each summary
     alignments = align(all_mentions_from_tsv, list(summaries.values()), all_mentions, data_folder=folders_with_pred_tsv, n_summaries=args.n_summaries , component=args.alignment_component)
 
-    # Enrich annotations in XML with summaries and in TSV with salience information
+    pred=extract_first_mentions(sc, alignments)
+    precision, recall, f1_score = calculate_scores(pred, gold_sal_ents)
+    print(f"Precision: {precision:.2f}, Recall: {recall:.2f}, F1 Score: {f1_score:.2f}")
+
+    # Add annotations in XML with summaries and in TSV with salience information
     add_summaries_to_xml(args.data_folder, summaries)
     add_anno_to_tsv(args.data_folder, alignments)
 
